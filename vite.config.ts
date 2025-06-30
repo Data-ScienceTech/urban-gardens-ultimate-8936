dbbe607da484862c8e33df9120e0a0057687b7ae
Carlos Denner <carlosdenner@gmail.com>
2025-06-30 14:42:50 -0400

    Set correct base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/urban-gardens-ultimate-8936/",
   base: process.env.VITE_BASE_PATH || '/',
   server: {
     host: "::",
